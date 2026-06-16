2b6374fa0559ca623cc874620d3fc13a69da5d88
Prerna Srivastava <48632112+prernasrivastav@users.noreply.github.com>
2026-06-17 00:40:43 +0530

    Add: remove-linked-list-elements - submission-1

---
+++ Data Structures & Algorithms/remove-linked-list-elements/submission-1.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        curr = dummy
+        while curr.next:
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return dummy.next
